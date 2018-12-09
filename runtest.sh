f7e8fb58a0699da3c6db6d95c1842e860c33ac5f
epauser <epauser@localhost.localdomain>
2018-12-09 17:14:26 +0000

    Co, N and idle time now being written to results.dat

--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 #script to run loadtest with different parameters
+
+# -e used with echo to enable interpretation of backslash escapes
+echo -e "Co\tN\tidle" > results.dat
 
 # n being the number of cocurrent users (also being used as the indexer)
 for n in {1..50} # loop 1 to 50 
@@ -9,5 +11,6 @@ do
 	idle=`mpstat 10 1 | awk 'END{print $12}'`
 	Co=`wc -l < synthetic.dat` # count the number of completed processes
 	pkill loadtest
-	echo $Co  $n  $idle
+	echo -e "$Co\t$n\t$idle" >> results.dat # appending results from tests ran to data file
+	echo "Load testing for $n cocurrent user complete"
 done
