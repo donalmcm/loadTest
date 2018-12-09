3bc7ac1a55c63be0a880ebd417a85e358155e154
epauser <epauser@localhost.localdomain>
2018-12-09 16:11:18 +0000

    ticks for loadtest commands not needed and have been removed

--- runtest.sh
+++ runtest.sh
@@ -5,9 +5,9 @@
 # n being the number of cocurrent users (also being used as the indexer)
 for n in {1..50} # loop 1 to 50 
 do
-	`./loadtest $n &`
+	./loadtest $n &  # running loadtest script in background using & 
 	idle=`mpstat 10 1 | awk 'END{print $12}'`
 	Co=`wc -l < synthetic.dat` # count the number of completed processes
-	`pkill loadtest`
-	echo $Co $n $idle
+	pkill loadtest
+	echo $Co  $n  $idle
 done
